68b6f05ff6d01f7eab35d2a8bd63623f4275946e
HervePerdry <herve.perdry@gmail.com>
2017-02-28 09:38:02 +0100

    creation

---
+++ R/burden.r
@@ -0,0 +1,12 @@
+
+burden <- function(x, regions, weights.1, weights.2 = weights.1) {
+  if(length(weights.1) != ncol(x) | length(weights.2) != ncol(x) | length(regions) != ncol(x)) {
+    stop("x and weights dimensions mismatch")
+  }
+  if(!is.factor(regions)) stop("'regions' is not a factor")
+
+  B <- .Call('oz_burden2', PACKAGE = 'oz', x@bed, nlevels(regions), regions, weights.1, weights.2)
+  colnames(B) <- levels(regions)
+  rownames(B) <- x@ped$id
+  B
+}
---
+++ src/burden.cpp
@@ -0,0 +1,82 @@
+// [[Rcpp::depends(RcppParallel)]]
+#include <Rcpp.h>
+#include <RcppParallel.h>
+#include <iostream>
+#include <ctime>
+#include "gaston.h"
+
+using namespace Rcpp;
+using namespace RcppParallel;
+
+NumericVector burden(matrix4 & A, NumericVector w1, NumericVector w2) { 
+  NumericVector B(A.true_ncol*4);  
+
+  for(size_t i = 0; i < A.nrow; i++) {
+    double ww[4] = {0, w1(i), w2(i), 0};
+    for(size_t j = 0; j < A.true_ncol; j++) {
+      uint8_t d = A.data[i][j];
+      B[4*j]   += ww[ (int) d&3 ];
+      B[4*j+1] += ww[ (int) ((d>>2)&3) ];
+      B[4*j+2] += ww[ (int) ((d>>4)&3) ];
+      B[4*j+3] += ww[ (int) ((d>>6)&3) ];
+    }
+  }
+  B = B[Range(0,A.ncol-1)];
+  return B;
+}
+
+NumericMatrix burden2(matrix4 & A, int n_regions, IntegerVector region, NumericVector w1, NumericVector w2) { 
+  NumericMatrix B(A.true_ncol*4, n_regions);  
+
+  for(size_t i = 0; i < A.nrow; i++) {
+    double ww[4] = {0, w1(i), w2(i), 0};
+    for(size_t j = 0; j < A.true_ncol; j++) {
+      uint8_t d = A.data[i][j];
+      B(4*j,   region[i]-1) += ww[ (int) d&3 ];
+      B(4*j+1, region[i]-1) += ww[ (int) ((d>>2)&3) ];
+      B(4*j+2, region[i]-1) += ww[ (int) ((d>>4)&3) ];
+      B(4*j+3, region[i]-1) += ww[ (int) ((d>>6)&3) ];
+    }
+  }
+  B = B(Range(0,A.ncol-1), _);
+  return B;
+}
+
+NumericVector burden(XPtr<matrix4> p_A, NumericVector w1, NumericVector w2) {
+  return burden(*p_A, w1, w2);
+}
+
+//[[Rcpp::export]]
+NumericMatrix burden2(XPtr<matrix4> p_A, int n_regions, IntegerVector region, NumericVector w1, NumericVector w2) {
+  return burden2(*p_A, n_regions, region, w1, w2);
+}
+
+
+
+ 
+RcppExport SEXP oz_burden(SEXP p_ASEXP, SEXP w1SEXP, SEXP w2SEXP) {
+BEGIN_RCPP
+    Rcpp::RObject rcpp_result_gen;
+    Rcpp::RNGScope rcpp_rngScope_gen;
+    Rcpp::traits::input_parameter< XPtr<matrix4> >::type p_A(p_ASEXP);
+    Rcpp::traits::input_parameter< NumericVector >::type w1(w1SEXP);
+    Rcpp::traits::input_parameter< NumericVector >::type w2(w2SEXP);
+    rcpp_result_gen = Rcpp::wrap(burden(p_A, w1, w2));
+    return rcpp_result_gen;
+END_RCPP
+}
+
+RcppExport SEXP oz_burden2(SEXP p_ASEXP, SEXP n_regionsSEXP, SEXP regionSEXP, SEXP w1SEXP, SEXP w2SEXP) {
+BEGIN_RCPP
+    Rcpp::RObject rcpp_result_gen;
+    Rcpp::RNGScope rcpp_rngScope_gen;
+    Rcpp::traits::input_parameter< XPtr<matrix4> >::type p_A(p_ASEXP);
+    Rcpp::traits::input_parameter< int >::type n_regions(n_regionsSEXP);
+    Rcpp::traits::input_parameter< IntegerVector >::type region(regionSEXP);
+    Rcpp::traits::input_parameter< NumericVector >::type w1(w1SEXP);
+    Rcpp::traits::input_parameter< NumericVector >::type w2(w2SEXP);
+    rcpp_result_gen = Rcpp::wrap(burden2(p_A, n_regions, region, w1, w2));
+    return rcpp_result_gen;
+END_RCPP
+}
+
