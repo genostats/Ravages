dcdc4ca6b0505295c1d87c083a6809a31194b0f2
Ozvan <ozvanbocher@hotmail.fr>
2017-02-28 17:22:11 +0100

    ajouté weights0

--- R/burden.r
+++ R/burden.r
@@ -1,11 +1,11 @@
 
-burden <- function(x, regions, weights.1, weights.2 = weights.1) {
+burden <- function(x, regions, weights.0, weights.1, weights.2) {
   if(length(weights.1) != ncol(x) | length(weights.2) != ncol(x) | length(regions) != ncol(x)) {
     stop("x and weights dimensions mismatch")
   }
   if(!is.factor(regions)) stop("'regions' is not a factor")
 
-  B <- .Call('oz_burden2', PACKAGE = 'oz', x@bed, nlevels(regions), regions, weights.1, weights.2)
+  B <- .Call('oz_burden2', PACKAGE = 'oz', x@bed, nlevels(regions), regions, weights.0, weights.1, weights.2)
   colnames(B) <- levels(regions)
   rownames(B) <- x@ped$id
   B
