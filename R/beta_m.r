c1d3031f23ec70d11efe0da90fb0544ce8812e65
HervePerdry <herve.perdry@gmail.com>
2017-03-24 17:01:20 +0100

    fonction sum_by_group

--- R/beta_m.r
+++ R/beta_m.r
@@ -2,5 +2,5 @@ Beta.M <- function(x, centre, region, which.snps = rep(TRUE, ncol(x))) {
   A <- minor.alleles.by.group(x, centre, which.snps)
   B <- major.alleles.by.group(x, centre, which.snps)
   Betam <- colSums(A**2)/colSums(A) + colSums(B**2)/colSums(B)
-  tapply(Betam, region[which.snps], sum)
+  .Call("oz_sum_by_group", PACKAGE = "oz", Betam, region[which.snps])
 }
