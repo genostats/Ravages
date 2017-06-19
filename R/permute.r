47ecff66d199b75033db804dde4e8daef1771ce7
Ozvan <ozvanbocher@hotmail.fr>
2017-06-19 12:39:16 +0200

    Rajout d'une option pour faire l'approximation de pearson dans permute

--- R/permute.r
+++ R/permute.r
@@ -12,9 +12,20 @@ permute <- function(x, centre, region, STAT, target = 10, B.max = 1e5, which.snp
   n.reg <- nlevels(region)
   A <- B <- S <- SC <- SC3 <- SC4 <- integer(n.reg)
   b <- 0;
+  
+  #Calcul de deux statistiques permutées si on veut réaliser une approximation par la loi de Pearson
+  if(pearson==TRUE){
+    a1 <- STAT(x, sample(centre), region, which.snps)
+    a2 <- STAT(x, sample(centre), region, which.snps)
+    K <- ( a1+a2 ) / 2
+    L <- abs(a1-a2)
+    L[which(L==0)] <- 1
+    Obs <- (Obs - K) / L
+  }
+              
   while(b < B.max) {
     b <- b+1
-    Perm <- STAT(x, sample(centre), region, which.snps);
+    ifelse(pearson==TRUE, Perm <- ( (STAT(x, sample(centre), region, which.snps) - K ) / L) , Perm <- STAT(x, sample(centre), region, which.snps) );
     w.greater <- (Perm >= Obs)
     A <- A + (!is.na(w.greater) & w.greater)
     B <- B + !is.na(w.greater)  
@@ -35,8 +46,22 @@ permute <- function(x, centre, region, STAT, target = 10, B.max = 1e5, which.snp
   M2 <- (1/B) * (SC - (S^2)/B)
   M3 <- (1/B) * SC3 - 3 * M2 * M - M^3
   M4 <- (1/B) * SC4 - 4 * M * (1/B) * SC3 + 6 * (1/B) * SC * M^2 - 3 * M^4
+  Sk <- M3 / (M2^(3/2))
+  Ku <- M4 /(M2^2)
   
   p <- ifelse(B==B.max, pnorm( ((Obs - M) / sqrt(V)), lower.tail=FALSE), A/B)
+
+  #Calcul de la p valeur avec approximation de Pearson
+  if(pearson==TRUE){
+    p <- ifelse(B==B.max, ppearson(Obs, pearsonFitM(M, V, Sk, Ku), lower.tail=FALSE), A/B)
+  }
   
-  data.frame(Obs = Obs, A = A, B = B, Moyenne = M, Variance = V, M2, M3, M4, p = p)
+  pval <- data.frame(Obs = Obs, A = A, B = B, Moyenne = M, Variance = V, M2, M3, M4, p = p)
+  
+  #Calcul de la p valeur de pearson quand NA
+  for (i in which(pval$p=="NaN")){
+    pval[i,"p"] <- ppearson(pval$Obs[i], pearsonFitM(pval$Moyenne[i], pval$Variance[i], pval$Sk[i], pval$Ku[i]), lower.tail=FALSE) 
+    }
+        
+  return(pval)
 }
