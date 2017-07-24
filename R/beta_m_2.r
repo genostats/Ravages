6b65cc3005ebf6d2209f199e6c80ecf9d189dbeb
Ozvan Bocher <obocher@med-laennec.univ-brest.fr>
2017-07-24 12:29:14 +0200

    Ajout du calcul de p

--- R/beta_m_2.r
+++ R/beta_m_2.r
@@ -1,4 +1,6 @@
 Beta.M.2 <- function(x, centre, region, which.snps = rep(TRUE, ncol(x)), target = 0, B.max = 0) {
   r <- .Call('oz_beta_m', PACKAGE = 'oz', x@bed, which.snps, region, centre, target, B.max) 
-  data.frame( genomic.region = levels(region), r )
+  res <- data.frame( genomic.region = levels(region), r )
+  res$p <- res$A/res$B
+  return(res)
 }
