859563ea3f157c68d0885a70beab36673d846bfd
Ozvan Bocher <obocher@med-laennec.univ-brest.fr>
2017-07-24 17:29:53 +0200

    Calcul de p pour CALPHA2

--- R/c_alpha_2.r
+++ R/c_alpha_2.r
@@ -1,5 +1,7 @@
 C.ALPHA.2 <- function(x, centre, region, which.snps = rep(TRUE, ncol(x)), target = 0, B.max = 0) {
   inverse <- (x@p > 0.5)
   r <- .Call('oz_c_alpha', PACKAGE = 'oz', x@bed, which.snps, region, centre, inverse, target, B.max) 
-  data.frame( genomic.region = levels(region), r )
+  res <- data.frame( genomic.region = levels(region), r )
+  res$p <- (res$A+1)/(res$B+1)
+  return(res)
 }
