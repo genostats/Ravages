b8a21263caf49b8b7176be99316405b858299cf7
HervePerdry <herve.perdry@gmail.com>
2017-07-25 07:47:58 +0200

    test if $A is defined before computing $p

--- R/beta_m_2.r
+++ R/beta_m_2.r
@@ -1,6 +1,6 @@
 Beta.M.2 <- function(x, centre, region, which.snps = rep(TRUE, ncol(x)), target = 0, B.max = 0) {
   r <- .Call('oz_beta_m', PACKAGE = 'oz', x@bed, which.snps, region, centre, target, B.max) 
   res <- data.frame( genomic.region = levels(region), r )
-  res$p <- (res$A+1)/(res$B+1)
+  if(!is.null(res$A)) res$p <- (res$A+1)/(res$B+1)
   return(res)
 }
--- R/c_alpha_2.r
+++ R/c_alpha_2.r
@@ -2,6 +2,6 @@ C.ALPHA.2 <- function(x, centre, region, which.snps = rep(TRUE, ncol(x)), target
   inverse <- (x@p > 0.5)
   r <- .Call('oz_c_alpha', PACKAGE = 'oz', x@bed, which.snps, region, centre, inverse, target, B.max) 
   res <- data.frame( genomic.region = levels(region), r )
-  res$p <- (res$A+1)/(res$B+1)
+  if(!is.null(res$A)) res$p <- (res$A+1)/(res$B+1)
   return(res)
 }
