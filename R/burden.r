5217cba3f32808aa75a5beb9369640dc51d93638
Ozvan <ozvanbocher@hotmail.fr>
2017-10-25 17:43:17 +0200

    Modification noms d'arguments

--- R/burden.r
+++ R/burden.r
@@ -1,8 +1,8 @@
-B_cast <- function(x, genomic_region, threshold=0.01){
+B_cast <- function(x, genomic_region, maf.threshold=0.01){
 
-  weights.0 <- ifelse( (x@snps$maf == (1-x@p) & x@snps$maf < threshold), 1, 0)
-  weights.1 <- ifelse( (x@snps$maf < threshold), 1, 0)
-  weights.2 <- ifelse( (x@snps$maf == x@p & x@snps$maf < threshold), 1, 0)
+  weights.0 <- ifelse( (x@snps$maf == (1-x@p) & x@snps$maf < maf.threshold), 1, 0)
+  weights.1 <- ifelse( (x@snps$maf < maf.threshold), 1, 0)
+  weights.2 <- ifelse( (x@snps$maf == x@p & x@snps$maf < maf.threshold), 1, 0)
       
   if(length(weights.0) != ncol(x) | length(weights.1) != ncol(x) | length(weights.2) != ncol(x) | length(genomic_region) != ncol(x)) {
     stop("x and weights dimensions mismatch")
