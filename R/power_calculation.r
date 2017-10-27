b4e0e161fe50af17e566ddbcd2befe510d28d5d2
Ozvan <ozvanbocher@hotmail.fr>
2017-10-27 17:05:37 +0200

    Nouvelle fonction pow2 et documentation pour les fonctions de simu

--- R/power_calculation.r
+++ R/power_calculation.r
@@ -57,51 +57,61 @@ pow <- function(maf, nb.replicate, nb.controls, nb.case, prot=FALSE, filter.maf,
   
   
 pow2 <- function(maf, nb.replicate, nb.groups, nb.controls, nb.case, prot=FALSE, filter.maf, nb.prot=0.05, nb.del=0.20){
-  respow <- data.frame(test=rep(c("CAST", "WSS", "CALPHA", "BETAM", "CASTP", "WSSP", "CALPHAP", "BETAMP"), each=9), nb_gpe=rep(c(3,2), each=36), OR=rep(seq(1,5, by=0.5), 8), puissance=rep(NA, 72))
+  respow <- data.frame(test=rep(c("CAST", "WSS", "CALPHA", "BETAM", "CASTP", "WSSP", "CALPHAP", "BETAMP"), each=9), nb_gps=rep(c(nb.groups,2), each=36), OR=rep(seq(1,5, by=0.5), 8), pow=rep(NA, 72))
   for (OR in seq(1,5, by=0.5)){
     res <- list(CAST=rep(NA, nb.replicate), WSS=rep(NA, nb.replicate), CALPHA=data.frame(A=rep(NA, nb.replicate), B=rep(NA, nb.replicate), p=rep(NA, nb.replicate)), BETAM=data.frame(A=rep(NA, nb.replicate), B=rep(NA, nb.replicate), p=rep(NA, nb.replicate)), CASTP=rep(NA, nb.replicate), WSSP=rep(NA, nb.replicate), CALPHAP=data.frame(A=rep(NA, nb.replicate), B=rep(NA, nb.replicate), p=rep(NA, nb.replicate)), BETAMP=data.frame(A=rep(NA, nb.replicate), B=rep(NA, nb.replicate), p=rep(NA, nb.replicate)))
     for (i in 1:nb.replicate){
         maf.controls <- mafs(maf, 1)$maf.controls
     if(prot==FALSE){
-        maf.case <- replicate(mafs(maf, sample(c(1,OR), length(maf), replace=TRUE, prob=c( (1 - nb.del), nb.del )))$maf.cases, nb.groups)
+        maf.case <- replicate(nb.groups, mafs(maf, sample(c(1,OR), length(maf), replace=TRUE, prob=c( (1 - nb.del), nb.del )))$maf.cases)
     }
     else{
-        maf.case <- replicate(mafs(maf, sample(c(1/OR,1,OR), length(maf), replace=TRUE, prob=c(nb.prot,(1 - nb.del - nb.prot), nb.del)))$maf.cases, nb.groups)
+        maf.case <- replicate(nb.groups, mafs(maf, sample(c(1/OR,1,OR), length(maf), replace=TRUE, prob=c(nb.prot,(1 - nb.del - nb.prot), nb.del)))$maf.cases)
     }
-      xt <- geno.simu.controls(maf.tem=maf.controls, nb.controls=nb.controls)
-      xc <- apply(maf.case, 2, geno.simu.case(maf.case=maf.case, nb.case))
-      xc@ped$id <- xc@ped$famid <- paste(rep(paste("C", seq(1, nb.groups), sep=""), nb.case), seq(1, nb.case), sep="_")
-      x <- rbind(xt, xc) ; x@ped$pheno <- c(rep(0, nb.controls), rep(seq(1,nb.groups), each=nb.case)) ; x@ped$pheno <- as.factor(x@ped$pheno)
+      xt <- geno.simu.controls(maf.controls=maf.controls, nb.controls=nb.controls)
+      xc <- apply(maf.case, 2, geno.simu.case, nb.case=nb.case)
+      xc[[1]]@ped$id <- xc[[1]]@ped$famid <- paste("C1", seq(1,nb.case), sep="")
+      x <- rbind(xt, xc[[1]])
+      for (j in 2:nb.groups){
+        xc[[j]]@ped$id <- xc[[j]]@ped$famid <- paste("C", j, seq(1, nb.case), sep="")
+        x <- rbind(x, xc[[j]])
+        }
+      x@ped$pheno <- c(rep(0, nb.controls), rep(seq(1,nb.groups), each=nb.case)) ; x@ped$pheno <- as.factor(x@ped$pheno) ; x <- set.stats(x, verbose=FALSE)
+      x@snps$chrpos <- paste(x@snps$chr, x@snps$pos, sep=":")
     if(filter.maf=="controls"){
-        x <- select.snps(x, select.inds(x, x@ped$pheno==0)@snps$maf<0.01)
+        x <- select.snps(x, select.inds(x, x@ped$pheno==0)@snps$maf < 0.01)
     }
     if(filter.maf=="sample"){
-        x <- select.snps(x, x@snps$maf<0.01)
+        x <- select.snps(x, x@snps$maf < 0.01)
     }
     if(filter.maf=="any"){
-        x <- select.snps(x, ( select.inds(x, x@ped$pheno==0)@snps$maf<0.01 | select.inds(x, x@ped$pheno==1)@snps$maf<0.01 | select.inds(x, x@ped$pheno==2)@snps$maf<0.01 ) )
+        snps <- select.snps(x, select.inds(x, x@ped$pheno==0)@snps$maf < 0.01)@snps$chrpos
+        for (k in 1:nb.groups){ 
+            snps <- union(snps,select.snps(x, select.inds(x, x@ped$pheno==k)@snps$maf < 0.01)@snps$chrpos)
+        }
+        x <- select.snps(x, (x@snps$chrpos %in% snps) )
     }
        
       res$CAST[i] <- Chi2( B_cast(x, as.factor(rep(1,ncol(x)))), x@ped$pheno)
       res$WSS[i] <- kruskal.test( WSS(x, as.factor(rep(1, ncol(x)))) , x@ped$pheno)$p.value
-      res$CALPHA[i,] <- C.ALPHA.2(x, centre=x@ped$pheno, region=as.factor(rep(1, ncol(x))), target=50, B.max=1000)[,c("A", "B", "p")]
-      res$BETAM[i,] <- Beta.M.2(x, centre=x@ped$pheno, region=as.factor(rep(1, ncol(x))), target=50, B.max=1000)[,c("A", "B", "p")]
+      res$CALPHA[i,] <- C.ALPHA.2(x, group=x@ped$pheno, genomic_region=as.factor(rep(1, ncol(x))), target=50, B.max=1000)[,c("A", "B", "p")]
+      res$BETAM[i,] <- Beta.M.2(x, group=x@ped$pheno, genomic_region=as.factor(rep(1, ncol(x))), target=50, B.max=1000)[,c("A", "B", "p")]
       
       x@ped$pheno <- c(rep(0, nb.controls), rep(1, nb.groups*nb.case)) ; x@ped$pheno <- as.factor(x@ped$pheno)
       res$CASTP[i] <- Chi2(B_cast(x, as.factor(rep(1,ncol(x)))), x@ped$pheno)
       res$WSSP[i] <- kruskal.test(WSS(x, as.factor(rep(1,ncol(x)))), x@ped$pheno)$p.value
-      res$CALPHAP[i,] <- C.ALPHA.2(x, centre=x@ped$pheno, region=as.factor(rep(1, ncol(x))), target=50, B.max=1000)[,c("A", "B", "p")]
-      res$BETAMP[i,] <- Beta.M.2(x, centre=x@ped$pheno, region=as.factor(rep(1, ncol(x))), target=50, B.max=1000)[,c("A", "B", "p")]
+      res$CALPHAP[i,] <- C.ALPHA.2(x, group=x@ped$pheno, genomic_region=as.factor(rep(1, ncol(x))), target=50, B.max=1000)[,c("A", "B", "p")]
+      res$BETAMP[i,] <- Beta.M.2(x, group=x@ped$pheno, genomic_region=as.factor(rep(1, ncol(x))), target=50, B.max=1000)[,c("A", "B", "p")]
     }
   
-  respow[which(respow$OR==OR & respow$test=="CAST"),"puissance"] <- sum(res$CAST<0.05)/nb.replicate
-  respow[which(respow$OR==OR & respow$test=="WSS"),"puissance"] <- sum(res$WSS<0.05)/nb.replicate
-  respow[which(respow$OR==OR & respow$test=="CALPHA"),"puissance"] <- sum(res$CALPHA[,"p"]<0.05)/nb.replicate
-  respow[which(respow$OR==OR & respow$test=="BETAM"),"puissance"] <- sum(res$BETAM[,"p"]<0.05)/nb.replicate
-  respow[which(respow$OR==OR & respow$test=="CASTP"),"puissance"] <- sum(res$CASTP<0.05)/nb.replicate
-  respow[which(respow$OR==OR & respow$test=="WSSP"),"puissance"] <- sum(res$WSSP<0.05)/nb.replicate
-  respow[which(respow$OR==OR & respow$test=="CALPHAP"),"puissance"] <- sum(res$CALPHAP[,"p"]<0.05)/nb.replicate
-  respow[which(respow$OR==OR & respow$test=="BETAMP"),"puissance"] <- sum(res$BETAMP[,"p"]<0.05)/nb.replicate
+  respow[which(respow$OR==OR & respow$test=="CAST"),"pow"] <- sum(res$CAST<0.05)/nb.replicate
+  respow[which(respow$OR==OR & respow$test=="WSS"),"pow"] <- sum(res$WSS<0.05)/nb.replicate
+  respow[which(respow$OR==OR & respow$test=="CALPHA"),"pow"] <- sum(res$CALPHA[,"p"]<0.05)/nb.replicate
+  respow[which(respow$OR==OR & respow$test=="BETAM"),"pow"] <- sum(res$BETAM[,"p"]<0.05)/nb.replicate
+  respow[which(respow$OR==OR & respow$test=="CASTP"),"pow"] <- sum(res$CASTP<0.05)/nb.replicate
+  respow[which(respow$OR==OR & respow$test=="WSSP"),"pow"] <- sum(res$WSSP<0.05)/nb.replicate
+  respow[which(respow$OR==OR & respow$test=="CALPHAP"),"pow"] <- sum(res$CALPHAP[,"p"]<0.05)/nb.replicate
+  respow[which(respow$OR==OR & respow$test=="BETAMP"),"pow"] <- sum(res$BETAMP[,"p"]<0.05)/nb.replicate
   }
   
   return(respow)}
