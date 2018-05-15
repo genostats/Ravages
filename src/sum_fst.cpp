66591027a1180a17b783b366e8c41c693b46de09
HervePerdry <herve.perdry@gmail.com>
2018-05-15 14:51:58 +0200

    Calculer le *vrai* fst

--- src/sum_fst.cpp
+++ src/sum_fst.cpp
@@ -23,22 +23,28 @@ class sumfst : public Stats {
     allelecounter X(&data[0], ncol, true_ncol, nb_snps, nb_ind_groups, ind_group);
     parallelReduce(0, nb_snps, X);
  
-    // calcul de la stat, d'abord par SNP sum n^2/(n+m)  et sum n+m 
-    std::vector<double> S1(nb_snps), S2(nb_snps);
+    // calcul de la stat, d'abord par SNP sum n^2/(n+m) et sum n+m  et sum n
+    std::vector<double> S1(nb_snps), S2(nb_snps), S3(nb_snps);
     for(size_t i = 0; i < nb_snps; i++) { // boucle sur les SNP
       for(size_t g = 0; g < nb_ind_groups; g++) { // sur les groupes d'invidus
         double n = (double) X.R[2*(i*nb_ind_groups + g)];   // = nb d'alleles 1 dans groupe g+1
         double m = (double) X.R[2*(i*nb_ind_groups + g)+1]; // = nb d'alleles 0 dans groupe g+1
         S1[i] += n*n/(n+m);
-        S2[i] += n+m; 
+        S2[i] += n+m;
+        S3[i] += n;
       }
     }
- 
+    // !!!! S2 et S3 pourraient être précalculés !!!! il faut juste insérer ce qu'il faut dans update_snps()
+    // S1[i]/S2[i] = \sum_g alpha_g p_g^2 où alpha_g = prop groupe g, p_g = freq groupe g
+    // S3[i]/S2[i] = p la fréquence globale 
     // calcule la somme des stats par région génomique
     for(int i = 0; i < nb_snp_groups; i++) stats[i] = 0;
     for(int i = 0; i < nb_snps; i++) {
-      if(S2[i] > 0) // pas de données !!
-        stats[ snp_group[i] - 1 ] += S1[i]/S2[i];
+      if(S2[i] > 0) { // pas de données !! 
+        double p = S3[i]/S2[i];
+        double p2 = p*p;
+        stats[ snp_group[i] - 1 ] += (S1[i]/S2[i] - p2)/(p-p2);
+      }
     }
   }
 
