6fc42ac486d4c27e50ebe72c4b8e442f6c5c36d0
HervePerdry <herve.perdry@gmail.com>
2018-08-01 11:03:46 +0200

    New function for random ber matrices

--- src/random_bed_matrix.cpp
+++ src/random_bed_matrix.cpp
@@ -33,13 +33,11 @@ XPtr<matrix4> random_bed_matrix(NumericMatrix maf, NumericVector size) {
   return p_A;
 }
 
-//[[Rcpp::export]]
 XPtr<matrix4> new_bed_matrix(int nrow, int ncol) {
   XPtr<matrix4> p_A(new matrix4(nrow, ncol));
   return p_A;
 }
 
-//[[Rcpp::export]]
 void random_filling_bed_matrix(XPtr<matrix4> p_A, NumericMatrix maf, NumericVector size, int firstrow) {  
   int nb_pop = maf.nrow();
   if(size.length() != nb_pop)
@@ -69,6 +67,54 @@ void random_filling_bed_matrix(XPtr<matrix4> p_A, NumericMatrix maf, NumericVect
     }
   }
 }
+
+
+//[[Rcpp::export]]
+void random_filling_bed_matrix_noHW(XPtr<matrix4> p_A, NumericMatrix proba_g0, NumericMatrix proba_g1, NumericVector size, int firstrow) {  
+  int nb_pop = size.length();
+  int nrow = proba_g0.ncol(); // nb snp
+  int ncol = sum(size);  // nb inds
+  if(proba_g0.nrow() != nb_pop || proba_g1.nrow() != nb_pop || nrow != proba_g1.ncol())
+    stop("Dimensions mismatch");
+  if(ncol != p_A->ncol || nrow + firstrow > p_A-> nrow) { 
+    stop("Dimensions mismatch");
+  }
+
+  // construction matrice proba_g0 + proba_g1
+  NumericMatrix pp2( clone(proba_g1) );
+  for(int i = 0; i < nb_pop; i++) {
+    for(int j = 0; j < nrow; j++) {
+      pp2(i,j) += proba_g0(i,j);
+      if( pp2(i,j) > 1.0 + 1e-14 ) 
+        stop("Sum of probas > 1!");
+    }
+  }
+
+  for(int i = 0; i < nrow; i++) {
+    int k = 0;
+    int row = firstrow + i;
+    for(int pop = 0; pop < nb_pop; pop++) {
+      double pr1 = proba_g0(pop,i); 
+      double pr2 = pp2(pop,i);
+      int s = size[pop];
+      for(int j = 0; j < s; j++) {
+        double r = Rf_runif(0.0, 1.0);
+        if(r < pr1) { 
+          p_A->set(row,k++,0);
+        } else if(r < pr2) {
+          p_A->set(row,k++,1);
+        } else {
+          p_A->set(row,k++,2);
+        }
+      }
+    }
+  }
+}
+
+
+
+
+
 
 RcppExport SEXP oz_random_bed_matrix(SEXP mafSEXP, SEXP sizeSEXP) {
 BEGIN_RCPP
@@ -103,4 +149,18 @@ BEGIN_RCPP
     return R_NilValue;
 END_RCPP
 }
+
+RcppExport SEXP oz_random_filling_bed_matrix_noHW(SEXP p_ASEXP, SEXP proba_g0SEXP, SEXP proba_g1SEXP, SEXP sizeSEXP, SEXP firstrowSEXP) {
+BEGIN_RCPP
+    Rcpp::RNGScope rcpp_rngScope_gen;
+    Rcpp::traits::input_parameter< XPtr<matrix4> >::type p_A(p_ASEXP);
+    Rcpp::traits::input_parameter< NumericMatrix >::type proba_g0(proba_g0SEXP);
+    Rcpp::traits::input_parameter< NumericMatrix >::type proba_g1(proba_g1SEXP);
+    Rcpp::traits::input_parameter< NumericVector >::type size(sizeSEXP);
+    Rcpp::traits::input_parameter< int >::type firstrow(firstrowSEXP);
+    random_filling_bed_matrix_noHW(p_A, proba_g0, proba_g1, size, firstrow);
+    return R_NilValue;
+END_RCPP
+}
+
 
