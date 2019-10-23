0d5bb44b13cbe0a8eaef71bc6a6772d314a246c7
Ozvan Bocher <obocher@valine.univ-brest.fr>
2019-10-23 17:19:25 +0200

    Add function to compute score matrix for bureden test, and function to take multiple annotations into account

---
+++ R/bed_matrix_split_genomic_region.r
@@ -0,0 +1,20 @@
+bed.matrix.split.genomic.region = function( x, changeID=TRUE, genomic.region=NULL, split.pattern="," )
+{
+  if (is.null(genomic.region) & !("genomic.region" %in% colnames(x@snps))) stop("genomic.region should be provided or already in x@snps")
+
+  if (!is.null(genomic.region)){
+    if(nrow(x@snps)!=length(genomic.region)) stop("genomic.region should have the same length as the number of markers in x")
+    if("genomic.region" %in% colnames(x@snps)) cat("genomic.region was already in x@snps, x@snps will be replaced.\n")
+    x@snps$genomic.region=genomic.region
+  }
+  genomic.region.split = strsplit(x@snps$genomic.region, split.pattern)
+  n.genomic.regions = sapply(genomic.region.split,length)
+  if (!any(n.genomic.regions>1)) 
+        stop("no any marker is annotated to multiple genomic regions")
+  nn = unlist(mapply( 1:length(n.genomic.regions) , FUN=rep , n.genomic.regions ))
+  bim = x@snps[nn,] ; row.names(bim)=NULL
+  bim$genomic.region = unlist(genomic.region.split)
+  if (changeID) { bim$id =  paste(bim$chr,bim$pos,bim$A1,bim$A2,bim$genomic.region,sep=":") }
+  y = as.bed.matrix( x=as.matrix(x)[,nn] , fam=x@ped , bim=bim ) 
+  return(y)
+}
---
+++ R/burden_weighted_matrix.r
@@ -0,0 +1,19 @@
+burden.weighted.matrix <- function(x, weights, genomic.region = x@snps$genomic.region){
+  if(length(weights) != length(genomic.region)) stop("weights and genomic.region should have the same length")
+
+  weights.0 <- ifelse( x@snps$maf > 0.5, 2*weights, 0)
+  weights.1 <- weights
+  weights.2 <- ifelse( x@snps$maf < 0.5, 2*weights, 0)
+
+  if(length(weights.0) != ncol(x) | length(weights.1) != ncol(x) | length(weights.2) != ncol(x) | length(genomic.region) != ncol(x)) {
+    stop("x and weights dimensions mismatch")
+  }
+  genomic.region <- as.factor(genomic.region)
+
+  B <- .Call('oz_burden2', PACKAGE = "Ravages", x@bed, nlevels(genomic.region), genomic.region, weights.0, weights.1, weights.2)
+  colnames(B) <- levels(genomic.region)
+  rownames(B) <- x@ped$id
+  return(B)
+}
+
+
