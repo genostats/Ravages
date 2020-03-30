fe549367645408c6018321336fba49fcd6d15e64
HervePerdry <herve.perdry@gmail.com>
2020-03-30 11:45:00 +0200

    tried a faster computation of the trace (does not work)

--- src/moments.cpp
+++ src/moments.cpp
@@ -10,9 +10,25 @@ List moments(NumericMatrix A, NumericMatrix P){
   NumericVector c1(4);
   c1[0] = A1.trace();
   c1[1] = A2.trace();
+
   c1[2] = (A1*A2).trace();
   c1[3] = (A2*A2).trace();
-  
+  // faster(?) computation of t(A^3) and t(A^4)
+  // THIS IS NOT FASTER
+  /* 
+  double t3 = 0, t4 = 0;
+  int n = A1.rows();
+  int m = A1.cols();
+  for(int j = 0; j < m; ++j)
+    for(int i = 0; i < n; ++i) {
+      double a2 = A2(i,j);
+      t3 += A1(j,i)*a2;
+      t4 += A2(j,i)*a2;
+    }
+  c1[2] = t3;
+  c1[3] = t4;
+  */
+
   double sigmaQ = sqrt(2*c1[1]);
   double s1 = c1[2]/pow(c1[1], 1.5);
   double s2 = c1[3]/pow(c1[1], 2);
