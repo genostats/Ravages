c4bb393b81483dea4e29df35af833857b3b58b61
Asmaa Ramadan <rasmaa99@yahoo.com>
2023-12-29 21:19:32 +0200

    Update adjustedCADD_annotation.r

--- R/adjustedCADD_annotation.r
+++ R/adjustedCADD_annotation.r
@@ -4,19 +4,10 @@ adjustedCADD.annotation <- function(x, SNVs.scores = NULL, indels.scores = NULL,
     if(is.null(indels.scores))  stop("Indels are present, 'indels.scores' should be provided with Phred scores 1.4 for Indels")
     if(!("SubRegion" %in% colnames(x@snps))) stop("The 'SubRegion' of each CADD region should be in x@snps, please use 'set.CADDregions()'")
     which.indels <- (nchar(x@snps$A1)>1 | nchar(x@snps$A2)>1)
-    x.indels <- select.snps(x, which.indels)
-    x.indels <- adjustedCADD.annotation.indels(x.indels, variant.scores = indels.scores, cores = cores, verbose = verbose, path.data = path.data)
-    if(ncol(x.indels)<ncol(x)){
-      x.SNVs <- select.snps(x, !which.indels)
-      x.SNVs <- adjustedCADD.annotation.SNVs(x.SNVs, variant.scores = SNVs.scores, cores = cores, verbose = verbose, path.data = path.data)
-      x@snps$adjCADD[which.indels] <- x.indels@snps$adjCADD
-      x@snps$adjCADD[!which.indels] <- x.SNVs@snps$adjCADD
-    }else{
-      x <- x.indels
-    }
-  }else{
+    #x.indels <- select.snps(x, which.indels)
+    #x.indels <- adjustedCADD.annotation.indels(x.indels, variant.scores = indels.scores, cores = cores, verbose = verbose, path.data = path.data)
     x <- adjustedCADD.annotation.SNVs(x, variant.scores = SNVs.scores, cores = cores, verbose = verbose, path.data = path.data)
-  }
+  
   x
 }
 
